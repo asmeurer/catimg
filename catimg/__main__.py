a057ce36499aa90ab5ae70b26eda2e91f89401ee
Aaron Meurer <asmeurer@gmail.com>
2015-07-13 22:08:45 -0500

    Note that we do filter out NSFW images

--- catimg/__main__.py
+++ catimg/__main__.py
@@ -6,9 +6,12 @@ in your terminal.
 
 NOTE: I do not own the images that you see, nor have I any control over
 them. You will see some image that is tagged as "cat" on Imgur. That could be
-anything. Use at your own risk.
+anything. We do filter out images that are tagged NSFW, but there are no
+guarantees that you won't see something you wish you hadn't. Use at your own
+risk.
 
 License: MIT
+
 """
 import sys
 import argparse
