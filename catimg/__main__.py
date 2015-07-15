b8a449620dd6fb9c424fd6bcad3812f535c345d0
Aaron Meurer <asmeurer@gmail.com>
2015-07-14 19:31:16 -0500

    Fix the grammar in other places too

--- catimg/__main__.py
+++ catimg/__main__.py
@@ -6,7 +6,7 @@ in your terminal.
 
 NOTE: I do not own the images that you see, nor have I any control over
 them. You will see some image that is tagged as "cat" on Imgur. That could be
-anything. We do filter out images that are tagged NSFW, but there are no
+anything. I do filter out images that are tagged NSFW, but there are no
 guarantees that you won't see something you wish you hadn't. Use at your own
 risk.
 
