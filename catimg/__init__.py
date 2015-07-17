0f301dc5405f53d5dbe37df71f146057d0a8441e
Aaron Meurer <asmeurer@gmail.com>
2015-07-16 20:01:40 -0500

    Use versioneer

--- catimg/__init__.py
+++ catimg/__init__.py
@@ -16,4 +16,6 @@ License: MIT
 from .iterm2 import *
 from .imgur import *
 
-__version__ = '1.0.2'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -5,18 +5,33 @@ if sys.version_info < (3,):
     sys.exit("catimg requires Python 3")
 
 from setuptools import setup
-import catimg
+import versioneer
 
 setup(
     name='catimg',
-    version=catimg.__version__,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='''Print an image of a cat from Imgur to iTerm2.''',
     author='Aaron Meurer',
     author_email='asmeurer@gmail.com',
     url='https://github.com/asmeurer/catimg',
     packages=['catimg'],
     package_data={'catimg.tests': ['aloha_cat.png']},
-    long_description=catimg.__doc__,
+    long_description="""
+catimg
+
+Uses iTerm2's proprietary escape codes and Imgur to display an image of a cat
+in your terminal.
+
+NOTE: I do not own the images that you see, nor have I any control over
+them. You will see some image that is tagged as "cat" on Imgur. That could be
+anything. I do filter out images that are tagged NSFW, but there are no
+guarantees that you won't see something you wish you hadn't. Use at your own
+risk.
+
+License: MIT
+
+""",
     entry_points={'console_scripts': [ 'catimg = catimg.__main__:main']},
     install_requires=[
         'requests',
